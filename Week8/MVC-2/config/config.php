4085d8178065806fbc132cdad2ab07a971a19d4e
Thăng Đào <daovanthang0604@gmail.com>
2022-06-10 10:46:37 +0700

    add week 8

---
+++ Week8/MVC-2/config/config.php
@@ -0,0 +1,16 @@
+<?php
+    
+    /** Configuration Variables **/
+    
+    const DEVELOPMENT_ENVIRONMENT = true;
+
+//    const DB_NAME = 'test';
+//    const DB_USER = 'thang';
+//    const DB_PASSWORD = 'thang';
+//    const DB_HOST = 'localhost';
+    
+    const DB_NAME = 'test';
+    const DB_USER = 'root';
+    const DB_PASSWORD = '';
+    const DB_HOST = '127.0.0.1';
+
---
+++ Week8/framework/config/config.php
@@ -0,0 +1,18 @@
+<?php
+    
+    /** Configuration Variables **/
+    
+    const DEVELOPMENT_ENVIRONMENT = true;
+    
+
+
+const DB_NAME = 'framework';
+//    const DB_NAME = 'framework';
+const DB_USER = 'thang';
+const DB_PASSWORD = 'thang';
+const DB_HOST = 'localhost';
+    
+    const BASE_PATH = 'http://localhost/WP20212/Week8/';
+    
+    
+    const PAGINATE_LIMIT = '5';
