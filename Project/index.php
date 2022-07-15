d2fa95c0a6a42f33f3ed9806507934803b73f4ea
krisspham <hpham2687@gmail.com>
2022-07-15 17:39:22 +0700

    feat: Add card feature

--- Project/index.php
+++ Project/index.php
@@ -154,23 +154,6 @@ include("includes/main.php");
         </div>
       </div>
 
-      <div class="page-footer__subline">
-        <div class="container clearfix">
-
-          <div class="copyright">
-            &copy; 2018 Avenue Fashion&trade;
-          </div>
-
-          <div class="developer">
-            Dev by Yasser Dalouzi
-          </div>
-
-          <div class="designby">
-            Design by Yasser Dalouzi
-          </div>
-
-        </div>
-      </div>
     </footer>
 </body>
 
